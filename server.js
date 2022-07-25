3ec656ef9052229f36996d7f0ce0b722ed8fb08f
Phantom Daldust <phantom.daldust@gmail.com>
2022-07-25 19:12:21 -0400

    sending index and show data

--- server.js
+++ server.js
@@ -4,11 +4,15 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
 app.get('/', (req, res) => {
   res.send('Welcome to an app about bread')
 })
 
-const breadsController= require('./controllers/breads_controller.js')
+const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
 app.listen(PORT, () => {
