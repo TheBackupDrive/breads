05efe7244ca2ab14457b14995619240b5257e4cf
Phantom Daldust <phantom.daldust@gmail.com>
2022-07-27 19:10:49 -0400

    delete route complete'

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -22,10 +22,11 @@ breads.get('/', (req, res) => {
 breads.get('/:arrayIndex', (req, res) => {
   if (Bread[req.params.arrayIndex]) {
     res.render('Show', {
-      bread:Bread[req.params.arrayIndex]
+      bread:Bread[req.params.arrayIndex],
+      index: req.params.arrayIndex,
     })
   } else {
-    res.send('404')
+    res.render('404')
   }
   // res.send(Bread)
 })
@@ -43,6 +44,12 @@ breads.post('/', (req, res) => {
   }
   Bread.push(req.body)
   res.redirect('/breads')
+})
+
+//DELETE
+breads.delete('/:indexArray', (req, res) => {
+  Bread.splice(req.params.indexArray, 1)
+  res.status(303).redirect('/breads')
 })
 
 module.exports = breads
--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require('react')
 const Default = require('./layouts/Default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
   console.log(bread.name)
     return (
       <Default>
@@ -17,7 +17,14 @@ function Show ({bread}) {
           have gluten
         </p>
         <img src={bread.image} alt={bread.names} />
+        <form action={`/breads/${index}?_method=DELETE`} method="POST">
+          <input type='submit' value="DELETE"/>
+        </form>
         <li><a href="/breads">Go home</a></li>
+        <form action={`/breads/${index}?_method=DELETE`} method="POST">
+          <input type='submit' value="DELETE"/>
+        </form>
+
       </Default>
     )
 }
