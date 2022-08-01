9285b7bb8dff0b66d284150924196d7b8a5246c7
Phantom Daldust <phantom.daldust@gmail.com>
2022-08-01 18:06:55 -0400

    edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -17,6 +17,14 @@ breads.get('/', (req, res) => {
     )
 })
 
+//EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+  res.render('edit', {
+    bread: Bread[req.params.indexArray],
+    index: req.params.indexArray
+  })
+})
+
 
 //SHOW
 breads.get('/:arrayIndex', (req, res) => {
@@ -50,6 +58,18 @@ breads.post('/', (req, res) => {
 breads.delete('/:indexArray', (req, res) => {
   Bread.splice(req.params.indexArray, 1)
   res.status(303).redirect('/breads')
+})
+
+//UPDATE
+breads.put ('/:arrayIndex', (req, res) => {
+  console.log('Put method')
+  if(req.body.hasGluten === 'on') {
+    req.body.hasGluten = true
+  } else {
+    req.body.hasGluten = false
+  }
+  Bread[req.params.arrayIndex] = req.body
+  res.redirect(`/breads/${req.params.arrayIndex}`)
 })
 
 module.exports = breads
