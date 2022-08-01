e204aa6dfaa7f07ba4fec2d9ef09f8d4d20a6ef4
Phantom Daldust <phantom.daldust@gmail.com>
2022-08-01 19:30:53 -0400

    Mongoose integrated and schema created

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+const mongoose = require('mongoose')
+
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
@@ -12,6 +14,9 @@ app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
 app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlparser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI)}
+)
 
 app.get('/', (req, res) => {
   res.send('Welcome to an app about bread')
